f6b503b9b949df3f8800b79f3aed188b038b0fd7
JakubGogola <jakubgogola97@gmail.com>
2019-03-28 14:08:38 +0100

    ada implemented

--- lista1/ada/src/corporation.adb
+++ lista1/ada/src/corporation.adb
@@ -12,7 +12,7 @@ package body Corporation is
    begin
       Ada.Numerics.Float_Random.Reset (G);
       Result := Ada.Numerics.Float_Random.Random (G);
-      Result := 5.0 * Result + 0.5; 
+      --Result := 2.0 * Result + 0.5; 
       return Duration (Result);
    end Get_Boss_Delay;
    
@@ -20,55 +20,61 @@ package body Corporation is
       
       -- Tasks
       task type Boss;
+      task type Listener;
       task type Worker (ID : Integer);
       type Worker_Access is access Worker;
       task type Client (ID : Integer);
       type Client_Access is access Client;
+      type Listener_Access is access Listener;
    
-      task type List_Server is
+      protected type List_Server is
 	 entry Add_New_Task (New_Task : in Corpo_Task);
 	 entry Get_Task (New_Task : out Corpo_Task);
+	 procedure Print_Tasks;
+      private
+	 Elements : Integer := 0;
+	 Tasks_List : Tasks_To_Do (0 .. Size_Of_Task_List - 1);
       end List_Server;
       
-      task type Magazine_Server is
+      protected type Magazine_Server is
 	 entry Add_New_Product (New_Product : in Product);
 	 entry Get_Product (New_Product : out Product);
+	 procedure Print_Products;
+      private
+	 Elements : Integer := 0; 
+	 Products_List : Products (0 .. Size_Of_Magazine - 1);
       end Magazine_Server;
       
-      -- Global variables
-      
-      -- Arrays
-      Tasks_List : Tasks_To_Do (0 .. Size_Of_Task_List - 1);
-      Products_List : Products (0 .. Size_Of_Magazine - 1);
-      
-      Is_Verbose_Mode_ON : Boolean := True;
-      
-      -- Implementations
-      
-      task body List_Server is 
-	 Elements : Integer := 0;
-      begin
-	 loop
-	    select
-	       when Size_Of_Task_List - Elements > 0 =>
-		  accept Add_New_Task (New_Task : in Corpo_Task) do
-		     Tasks_List (Elements) := New_Task;
-		     Elements := Elements + 1;
-		  end Add_New_Task;
-		  
-	    or
-	       when Elements > 0 =>
-		  accept Get_Task (New_Task : out Corpo_Task) do
-		     New_Task := Tasks_List (Elements - 1);
-		     Elements := Elements - 1;
-		  end Get_Task;
-	    or
-	       terminate;
-	    end select;
-	    
-	 end loop;
+      protected body List_Server is 
+	 entry Add_New_Task (New_Task : in Corpo_Task)  
+	      when Size_Of_Task_List - Elements > 0 is
+	 begin
+	    Tasks_List (Elements) := New_Task;
+	    Elements := Elements + 1;
+	 end Add_New_Task;
+	 
+	 entry Get_Task (New_Task : out Corpo_Task) 
+	      when Elements > 0 is
+	 begin
+	    New_Task := Tasks_List (Elements - 1);
+	    Elements := Elements - 1;
+	 end Get_Task;
+	 
+	 procedure Print_Tasks is 
+	 begin
+	    if Elements = 0 then
+	       Put_Line ("List of tasks is empty!");
+	    else
+	       Put_Line ("Tasks: ");
+	       for I in Tasks_List'First .. Elements - 1 loop
+		  Put_Line (I'Image & ": " & Tasks_List (I).First_Arg'Image & Tasks_List (I).Operator
+	      & Tasks_List (I).Second_Arg'Image);
+	       end loop;
+	    end if;
+	 end Print_Tasks;
       end List_Server;
       
+
       List : List_Server;
    
       task body Boss is 
@@ -116,35 +122,41 @@ package body Corporation is
 	    delay Get_Boss_Delay;
 	 end loop;
       end Boss;
-      
-      task body Magazine_Server is 
-	 Elements : Integer := 0;
-      begin
-	 loop
-	    select
-	       when Size_Of_Magazine - Elements > 0 =>
-		  accept Add_New_Product (New_Product : in Product) do
-		     Products_List (Elements) := New_Product;
-		     Elements := Elements + 1;
-		     
-		     if Is_Verbose_Mode_ON then
-			Put_Line ("Product was added to magazine with value: " & New_Product.Product_Value'Image);
-		     end if;
-		     
-		     
-		  end Add_New_Product;
-	    or
-	       when Elements > 0 =>
-		  accept Get_Product (New_Product : out Product) do
-		     New_Product := Products_List (Elements - 1);
-		     Elements := Elements - 1;
-		  end Get_Product;
-	    or
-	       terminate;
-	    end select;
-	 end loop;
+
+      protected body Magazine_Server is 
+	 entry Add_New_Product (New_Product : in Product)
+	      when Size_Of_Magazine - Elements > 0 is
+	 begin
+	    Products_List (Elements) := New_Product;
+	    Elements := Elements + 1;
+	    
+	    if Is_Verbose_Mode_ON then
+	       Put_Line ("Product was added to magazine with value: " & New_Product.Product_Value'Image);
+	    end if;
+	    
+	 end Add_New_Product;
+	 
+	 entry Get_Product (New_Product : out Product) 
+	      when Elements > 0 is
+	 begin
+	    New_Product := Products_List (Elements - 1);
+	    Elements := Elements - 1;
+	 end Get_Product;
+	 
+	 procedure Print_Products is 
+	 begin
+	    if Elements = 0 then
+	       Put_Line ("Magazine is empty!");
+	    else
+	       Put_Line ("Products: ");
+	       for I in Products_List'First .. Elements - 1 loop
+		  Put_Line (I'Image & ": " & Products_List (I).Product_Value'Image);
+	       end loop;
+	    end if;
+	 end Print_Products;
+	 
       end Magazine_Server;
-      
+         
       Magazine : Magazine_Server;
       
       task body Worker is
@@ -185,17 +197,47 @@ package body Corporation is
       begin
 	 loop
 	    Magazine.Get_Product (New_Product);
-	    Put_Line ("Client " & ID'Image & " has already bought product with value: " & New_Product.Product_Value'Image);
+	    
+	    if Is_Verbose_Mode_ON then
+	       Put_Line ("Client " & ID'Image 
+		  & " has already bought product with value: " & New_Product.Product_Value'Image);
+	    end if;
 	    
 	    delay Client_Delay;
 	 end loop;
       end Client;
       
+      task body Listener is 
+	 Command : Character;
+      begin 
+	 Put_Line ("Usage (avaiable commands): ");
+	 Put_Line ("m - print list of products stored in magazine");
+	 Put_Line ("t - print list of tasks to do");
+	 
+	 loop
+	    Get (Command);
+	    
+	    case Command is 
+	    when 't' =>
+	       List.Print_Tasks;
+	    when 'm' =>
+	       Magazine.Print_Products;
+	    when others =>
+	       Put_Line ("Unkown command");
+	    end case;
+	 end loop;
+      end Listener;
+     
       New_Boss : Boss;
       New_Worker : Worker_Access;
       New_Client : Client_Access;
+      New_Listener : Listener_Access;
       
    begin
+      if not Is_Verbose_Mode_ON then
+	 New_Listener := new Listener;
+      end if;
+      
       for I in 1 .. Num_Of_Workers loop
 	 New_Worker := new Worker (I);
       end loop;
--- lista1/ada/src/parameters.ads
+++ lista1/ada/src/parameters.ads
@@ -5,14 +5,16 @@ package Parameters is
    Size_Of_Magazine  : constant Integer := 10;
    
    -- Delays
-   Worker_Delay      : constant Duration := 5.0;
-   Client_Delay      : constant Duration := 6.0;
+   Worker_Delay      : constant Duration := 1.5;
+   Client_Delay      : constant Duration := 2.0;
    Boss_Delay        : constant Duration := 1.0; -- Temporary solution - should be random
    
    
    -- Numbers
    Num_Of_Workers    : constant Integer := 4;
-   Num_Of_Clients    : constant Integer := 8;
+   Num_Of_Clients    : constant Integer := 6;
    
+   -- Verbose mode flag
+   Is_Verbose_Mode_ON : Boolean := True;
 
 end Parameters;
